b25c3143732ca7badaafe234d1cd4a4fdc0a66bd
Светка <sveta.chakrygina@mail.ru>
2022-10-19 21:19:23 +0300

    Домашнее задание 3.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-    human Maxim = new human("Максим",1987,"Минск", "бренд-менеджера");
+    human Maxim = new human("Максим",-1987,"Минск", "бренд-менеджера");
     human Anya= new human("Аня", 1993,"Москва", "методиста образовательных программ");
-    human Katya = new human("Катя", 1994,"Калининград", "продакт-менеджера");
-    human Artem = new human("Артем", 1995,"Москва", "директора по развитию бизнеса");
-
+    human Katya = new human("Катя", 1994,"Калининград", null);
+    human Artem = new human("Артем", 1995,null, "директора по развитию бизнеса");
 
 
         Maxim.hello();
--- src/human.java
+++ src/human.java
@@ -5,14 +5,28 @@ public class human {
     String job;
 
     public human(String name, int yearOfBirth, String town, String job) {
+        if (name == null) {
+            this.name = "Информация отсутсвует";
+        }else{
         this.name = name;
-        this.yearOfBirth = yearOfBirth;
-        this.town = town;
-        this.job = job;
+        }
+        if (yearOfBirth <= 0) {
+            this.yearOfBirth = 0;
+        }else{
+            this.yearOfBirth = yearOfBirth;
+        }
+        if (town == null) {
+            this.town = "Информация отсутсвует";
+        }else{
+            this.town = town;
+        }
+        if (job == null) {
+            this.job = "Информация отсутсвует";
+        }else{
+            this.job = job;
+        }
     }
-
-    void hello() {
-        System.out.println("Привет! Меня зовут " + name + " я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
-    }
-
-}
+            void hello() {
+                System.out.println("Привет! Меня зовут " + name + " я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
+            }
+        }
