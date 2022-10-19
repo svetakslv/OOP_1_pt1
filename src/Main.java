e349532a3fab464f66a0b1c1a69af18e8df419dd
Светка <sveta.chakrygina@mail.ru>
2022-10-19 20:16:17 +0300

    Первая и вторая задачи

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-    human Maxim = new human("Максим",1987,"Минск");
-    human Anya= new human("Аня", 1993,"Москва");
-    human Katya = new human("Катя", 1994,"Калининград");
-    human Artem = new human("Артем", 1995,"Москва");
+    human Maxim = new human("Максим",1987,"Минск", "бренд-менеджера");
+    human Anya= new human("Аня", 1993,"Москва", "методиста образовательных программ");
+    human Katya = new human("Катя", 1994,"Калининград", "продакт-менеджера");
+    human Artem = new human("Артем", 1995,"Москва", "директора по развитию бизнеса");
 
 
 
--- src/human.java
+++ src/human.java
@@ -2,15 +2,17 @@ public class human {
     String name;
     int yearOfBirth;
     String town;
+    String job;
 
-    public human(String name, int yearOfBirth, String town) {
+    public human(String name, int yearOfBirth, String town, String job) {
         this.name = name;
         this.yearOfBirth = yearOfBirth;
         this.town = town;
+        this.job = job;
     }
 
     void hello() {
-        System.out.println("Привет! Меня зовут " + name + " я из города " + town + ". Я родился в " + yearOfBirth + " году. Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + " я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
     }
 
 }
