6fc12b8706a38dc0f3a6365a08bddecfc08ae896
Светка <sveta.chakrygina@mail.ru>
2022-10-22 20:13:06 +0300

    Домашнее задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-    human Maxim = new human("Максим",-1987,"Минск", "бренд-менеджера");
+    human Maxim = new human("Максим",1987,"Минск", "бренд-менеджера");
     human Anya= new human("Аня", 1993,"Москва", "методиста образовательных программ");
     human Katya = new human("Катя", 1994,"Калининград", null);
-    human Artem = new human("Артем", 1995,null, "директора по развитию бизнеса");
+    human Artem = new human("Артем", 1995,"", "директора по развитию бизнеса");
+    human Vladimir = new human("Владимир", 2001, "Казань", "без работы");
 
+    printInfo(Maxim);
+    printInfo(Anya);
+    printInfo(Katya);
+    printInfo(Artem);
+    printInfo(Vladimir);
 
-        Maxim.hello();
-        Anya.hello();
-        Katya.hello();
-        Artem.hello();
+    }
+    private static void printInfo(human human) {
+        System.out.println("Привет! Меня зовут " + human.name + " я из города " + human.getTown() + ". Я родился в " + human.getYearOfBirth() + " году. Я работаю на должности " + human.job + ". Будем знакомы!");
     }
 }
--- src/human.java
+++ src/human.java
@@ -1,32 +1,54 @@
 public class human {
     String name;
-    int yearOfBirth;
-    String town;
+    private int yearOfBirth;
+    private String town;
     String job;
+
 
     public human(String name, int yearOfBirth, String town, String job) {
         if (name == null) {
             this.name = "Информация отсутсвует";
-        }else{
-        this.name = name;
+        } else {
+            this.name = name;
         }
         if (yearOfBirth <= 0) {
             this.yearOfBirth = 0;
-        }else{
+        } else {
             this.yearOfBirth = yearOfBirth;
         }
         if (town == null) {
             this.town = "Информация отсутсвует";
-        }else{
+        } else {
             this.town = town;
         }
         if (job == null) {
             this.job = "Информация отсутсвует";
-        }else{
+        } else {
             this.job = job;
         }
     }
-            void hello() {
-                System.out.println("Привет! Меня зовут " + name + " я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
-            }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth <= 0) {
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth;
         }
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        if (town == null) {
+            this.town = "Информация отсутсвует";
+        } else {
+            this.town = town;
+        }
+    }
+}
